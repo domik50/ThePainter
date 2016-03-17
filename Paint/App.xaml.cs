e459e48a152c05859ad91d265033cbcdd194d0bb
dominik <domikfil@gmail.com>
2016-03-17 19:21:12 +0100

    zmiana

--- Paint/App.xaml.cs
+++ Paint/App.xaml.cs
@@ -7,10 +7,5 @@ using System.Windows;
 
 namespace Paint2
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
     }
 }
